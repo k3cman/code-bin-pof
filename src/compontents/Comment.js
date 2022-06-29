28b2adb0af497f72fa882b13695ef1ec3fb0ea4e
k3cman <kecman.dev@gmail.com>
2022-06-29 12:13:01 +0200

    Style comment boxes

--- src/compontents/Comment.js
+++ src/compontents/Comment.js
@@ -1,19 +1,31 @@
 import React, { useState } from 'react'
 
-function Comment({comment, changed}) {
+import './Comment.css'
+
+function Comment({ comment, changed }) {
 
   const [areaComment, setAreaComment] = useState('');
   console.log(comment)
-  return (
-    <>
-     {comment === 'INSERT' ? (<textarea value={areaComment} onChange={(e) => {
-      setAreaComment(e.target.value)
-     }} /> ) : (<span>{comment}</span>)}
-     {comment === 'INSERT' ? (
-      <button onClick={() => changed(areaComment)}>Submit</button>
-     ) : null}
-    </>
-  )
+
+  if (comment !== undefined) {
+    return (
+      <div class="single-comment text-gray-600 text-sm">
+        {comment === 'INSERT' ? (
+        <textarea 
+        className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 " placeholder="Your message..."
+        value={areaComment} onChange={(e) => {
+          setAreaComment(e.target.value)
+        }} />) : (<span>{comment}</span>)}
+        {comment === 'INSERT' ? (
+          <div className="flex justify-end mt-1">
+            <button className="bg-gray-700 hover:bg-gray-900 text-white font-bold py-1 px-4 rounded" onClick={() => changed(areaComment)}>Submit</button>
+          </div>
+          ) : null}
+      </div>
+    )
+  }
+
+  return null;
 }
 
 export default Comment
--- src/compontents/Highighter.js
+++ src/compontents/Highighter.js
@@ -3,9 +3,10 @@ import SyntaxHighlighter from 'react-syntax-highlighter';
 import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 import Comment from './Comment';
+import './Highlighter.css'
 
-const codeChallenge = 
-`import { useState, useEffect } from 'react'
+const codeChallenge =
+    `import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 interface Message {
@@ -26,12 +27,12 @@ export const useChatData = (roomId: string) => {
 
 const Highighter = () => {
 
-    
 
-    const [comments, setComments] = useState([{lineNo:3, comment: 'example'}, {lineNo:5, comment: 'example2'}]);
+
+    const [comments, setComments] = useState([{ lineNo: 3, comment: 'example' }, { lineNo: 5, comment: 'example2' }]);
 
     const [codeFragments, setCodeFragments] = useState([])
-  
+
 
     useEffect(() => {
         // Map items
@@ -42,7 +43,7 @@ const Highighter = () => {
         let lastLine = 0;
         lineNumbers.forEach((line, index) => {
             result.push({
-                code:codeChallengeArr.slice(lastLine, line).join('\n'),
+                code: codeChallengeArr.slice(lastLine, line).join('\n'),
                 comment: comments[index].comment || undefined,
                 commentIndex: index
             })
@@ -58,7 +59,7 @@ const Highighter = () => {
 
 
     const updateComment = (value, codeFragmentIndex) => {
-        if(codeFragmentIndex){
+        if (codeFragmentIndex) {
             const commentState = comments;
             commentState[codeFragmentIndex].comment = value;
 
@@ -71,40 +72,41 @@ const Highighter = () => {
         const code = previousFragments.map(one => one.code);
         let lineNo = 0;
         code.forEach((item) => lineNo += item.split('\n').length)
+
         return lineNo + lineNumber;
     }
 
-  return (
-    <>
-    {codeFragments.map((codeFragment, index) => {
-       return ( <div key={index}>
-            <SyntaxHighlighter language='typescript' style={atomOneDark}
-            showInlineLineNumbers={true}
-            wrapLines={true}
-            showLineNumbers={true}
-            lineProps={(lineNumber) => ({
-                style: {cursor: 'pointer'},
-                onClick(){
-                    console.log(index)
-                    setComments([...comments, {
-                        lineNo: index === 0 ? lineNumber : getLineNumber(index, lineNumber),
-                        comment: 'INSERT'
-                    }])
-                }
+    return (
+        <>
+            {codeFragments.map((codeFragment, index) => {
+                return (<div key={index}>
+                    <SyntaxHighlighter language='typescript' style={atomOneDark}
+                        showInlineLineNumbers={true}
+                        wrapLines={true}
+                        showLineNumbers={true}
+                        lineProps={(lineNumber) => ({
+                            style: { cursor: 'pointer' },
+                            onClick() {
+                                console.log(index)
+                                setComments([...comments, {
+                                    lineNo: index === 0 ? lineNumber : getLineNumber(index, lineNumber),
+                                    comment: 'INSERT'
+                                }])
+                            }
+                        })}
+                    >
+                        {codeFragment.code}
+                    </SyntaxHighlighter>
+                    <div className="comment">
+                        <Comment comment={codeFragment.comment} changed={(e) => updateComment(e, codeFragment.commentIndex)} />
+                    </div>
+
+                </div>)
             })}
-        >
-            {codeFragment.code}
-        </SyntaxHighlighter>
-        <div className="comment">
-            <Comment comment={codeFragment.comment} changed={(e) => updateComment(e, codeFragment.commentIndex)}/>
-            </div>
-        
-        </div>) 
-    })}
-        
-        
-    </>
-  )
+
+
+        </>
+    )
 }
 
 export default Highighter
